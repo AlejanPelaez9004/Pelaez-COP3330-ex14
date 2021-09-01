7052cdd05730bb254ecab6ed50727e8d40895238
AlejanPelaez9004 <alejanpelaez@knights.ucf.edu>
2021-09-01 15:16:28 -0700

    Completed!

--- src/main/java/Pelaez/Alejandro/App.java
+++ src/main/java/Pelaez/Alejandro/App.java
@@ -8,10 +8,25 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args)
     {
-        System.out.print("What is your name?");
         Scanner input = new Scanner(System.in);
-        String name = input.next();
-        System.out.print("Hello " + name + ", nice to meet you!");
+        System.out.print("What is the amount? ");
+        float amount = input.nextFloat();
+        System.out.print("What is the state? ");
+        String state = input.next();
+        float tax = 0.55f;
+        float total;
+        if (state.equalsIgnoreCase("WI"))
+        {
+            total = (amount + tax);
+            System.out.printf("The subtotal is: $%.2f\n" +
+                    "The tax is: $%.2f\n" +
+                    "The total is: $%.2f", amount, tax, total);
+            System.out.print("\n");
+            return;
+        }
+
+        total = amount;
+        System.out.printf("The total is: $%.2f", total);
         System.out.print("\n");
     }
 }
